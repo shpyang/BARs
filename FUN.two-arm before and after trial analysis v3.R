8ebce03a12dd2c34ba37611d402c6cc78aeb9c67
shpyang <58001750+shpyang@users.noreply.github.com>
2023-11-27 14:16:03 -0600

    Add files via upload

--- FUN.two-arm before and after trial analysis v3.R
+++ FUN.two-arm before and after trial analysis v3.R
@@ -5,8 +5,9 @@ require(haven)
 library(emmeans)
  
 
-FUN.BA=function(data, outcome.name, outcome.before, outcome.after, id.name, trt.name, trt.levels, covariates, rd, rd2)
-{ if (missing(rd)) {rd=2}
+FUN.BA=function(data, outcome.name, outcome.before, outcome.after, id.name, trt.name, trt.levels, covariates, rd, rd2, LTransformed)
+{ if (missing(LTransformed)) {LTransformed=FALSE} 
+  if (missing(rd)) {rd=2}
   if (missing(rd2)) {rd2=1}
   data$outcome.before=data[,outcome.before]
 data$outcome.after =data[,outcome.after] 
@@ -58,9 +59,18 @@ ddifft=t(as.matrix(mout[[10]][row.names(mout[[10]])==paste0("trt", trt.levels[2]
 ddiff=rbind(t(c("Reference", "")), c(paste0(round(ddifft[1,1],rd2), "(", round((ddifft[1,1]-qt(.975, ddifft[,3])*ddifft[1,2]),rd2), ", ",
                                                round((ddifft[1,1]+qt(.975, ddifft[,3])*ddifft[1,2]),rd2), ")"), round(ddifft[1,5],3)))
 
-
+if (LTransformed) {
+  ddiff=rbind(t(c("Reference", "")), c(paste0(round(exp(ddifft[1,1]),rd2), "(", round(exp(ddifft[1,1]-qt(.975, ddifft[,3])*ddifft[1,2]),rd2), ", ",
+                                              round(exp(ddifft[1,1]+qt(.975, ddifft[,3])*ddifft[1,2]),rd2), ")"), round(ddifft[1,5],3)))
+          }
 out=as.data.frame(cbind(c(outcome.name, ""), wtrt, mnout, mdout, ddiff))
 names(out)=c("Outcome.var", "Treatment", "Baseline", "Change", "Difference", "P value")
+
+if (LTransformed) {
+  
+  names(out)=c("Outcome.var", "Treatment", "Baseline of log2-transformed", "Change of log2-transformed", "Intervention main effect", "P value")
+  
+}
 return(out)
 }
  
